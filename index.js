9704cbb32035456f30fca196a819b8d559804d6c
Hans Olave <hansolave@gmail.com>
2025-06-03 14:48:59 -0500

    Modifique index.js y creamos la conección a base de datos

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('El Servidor esta Funcionando!');
-    console.log('El Servidor esta Funcionando!');
+
+const postRoute = require('./routes/post.js');
+app.use('/servicios', postRoute);
+
+mongoose.connect('mongodb+srv://hansolavet:AvlaW6BVwcp9AdP7@api01.0j5gidv.mongodb.net/mi-post?retryWrites=true&w=majority&appName=Api01', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 
 });
-
 
 app.listen(15000);
